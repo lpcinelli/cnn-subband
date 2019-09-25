0ff13ef18082614ca79d6de7261d1305aff466b0
lpcinelli <lpcinelli@gmail.com>
2019-09-24 22:24:50 -0300

    add cifar100

--- datasets.py
+++ datasets.py
@@ -1,4 +1,3 @@
-import pdb
 
 import numpy as np
 import torch
@@ -41,7 +40,7 @@ class Cifar10():
         default_transforms = transforms.Compose(Cifar10.default_transform)
         extra_transforms = transforms.Compose(extra_transforms +
                                               Cifar10.default_transform)
-        # pdb.set_trace()
+
         train_set = datasets.CIFAR10(root=data_dir,
                                      train=True,
                                      download=True,
@@ -68,12 +67,64 @@ class Cifar10():
                                                        shuffle=False,
                                                        num_workers=num_workers)
 
-        # @property
-        # def class_names(self):
-        #     return ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog',
-        #             'horse', 'ship', 'truck')
-        self.class_names = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog',
-                            'horse', 'ship', 'truck')
+class Cifar100():
+    normalize = transforms.Normalize(
+        mean=(0.5071, 0.4865, 0.4409),
+        std=(0.2673, 0.2564, 0.2762)
+    )
+
+    default_transform = [transforms.ToTensor(), normalize]
+    def __init__(self,
+                 data_dir='./data',
+                 batch_size=32,
+                 split=0.1,
+                 rnd_seed=0,
+                 num_workers=4,
+                 extra_transforms=[]):
+        """Create dataloaders for training and test sets of CIFAR-100 dataset. Download it if
+        necessary.
+
+        Args:
+            batch_size (int, optional): Nb of img elements of each mini-batch. Defaults to 32.
+            num_workers (int, optional): Nb of works to fetch data. Defaults to 4.
+            extra_transforms (list, optional): Transformations to apply to the imgs. Defaults to [].
+        """
+
+        assert isinstance(extra_transforms,
+                          list), 'extra_transforms should be a list'
+        assert np.all([extra in available_extra.keys()
+                      for extra in extra_transforms]), 'extra_transforms should be a list'
+
+        extra_transforms = []
+        default_transforms = transforms.Compose(Cifar100.default_transform)
+        extra_transforms = transforms.Compose(extra_transforms +
+                                              Cifar100.default_transform)
+
+        train_set = datasets.CIFAR100(root=data_dir,
+                                     train=True,
+                                     download=True,
+                                     transform=extra_transforms)
+        val_set = datasets.CIFAR100(root=data_dir,
+                                   train=True,
+                                   download=True,
+                                   transform=default_transforms)
+
+        self.train_loader, self.valid_loader = get_train_valid_loader(
+            train_set,
+            val_set,
+            valid_size=split,
+            batch_size=batch_size,
+            num_workers = num_workers,
+            rnd_seed=rnd_seed)
+
+        test_set = datasets.CIFAR100(root=data_dir,
+                                    train=False,
+                                    download=True,
+                                    transform=default_transforms)
+        self.test_loader = torch.utils.data.DataLoader(test_set,
+                                                       batch_size=batch_size,
+                                                       shuffle=False,
+                                                       num_workers=num_workers)
 
 
 def get_train_valid_loader(train_dataset,
@@ -113,3 +164,9 @@ def get_train_valid_loader(train_dataset,
     )
 
     return train_loader, valid_loader
+
+
+dataset = {
+    'cifar10': Cifar10,
+    'cifar100': Cifar100,
+}
